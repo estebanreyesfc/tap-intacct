4b46750242d473ebff752735d8357b3c23c729c6
estebanreyesfc <esteban.reyes@freshconsulting.com>
2026-02-05 11:14:59 -0600

    Update __init__.py

--- tap_intacct/__init__.py
+++ tap_intacct/__init__.py
@@ -29,7 +29,6 @@ def stream_is_selected(mdata):
 def do_sync(config, catalog, state):
     LOGGER.info('Starting sync.')
     target = config.get("only_stream")
-    print(target)
 
     for stream in catalog['streams']:
         stream_name = stream['tap_stream_id']
